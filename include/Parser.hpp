f3a0ef45f0ce59dcd6d4f9f10be96147066e0b74
oussama7007 <oussamatachakourt@gmail.com>
2026-06-21 17:22:32 +0100

    [Si HAMOU] 2026-06-21 17:22 | Modified: include/Parser.hpp

---
+++ include/Parser.hpp
@@ -0,0 +1,25 @@
+
+
+
+
+#paragma once 
+
+
+#include <string>
+#include <vector>
+
+
+
+struct Message
+{
+    std::string     prefix;
+    std::string     command;
+    std::vector<std::string> params;
+};
+
+class Parser 
+{
+    public:
+    static Message parse(const std::string &raw_line);
+}
+
