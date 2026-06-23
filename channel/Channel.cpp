1827e50340245594dc025b9e75f99dc4dabf6fcf
oussama7007 <oussamatachakourt@gmail.com>
2026-06-23 17:00:23 +0100

    [Si HAMOU] 2026-06-23 17:00 | Modified: channel/Channel.cpp,include/Channel.hpp

---
+++ channel/Channel.cpp
@@ -0,0 +1,61 @@
+#include "include/Channel.hpp"
+#include <algorithm> 
+
+Channel::Channel(const std::string &name) 
+    : _name(name), _topic(""), _isInviteOnly(false), 
+      _topicRestricted(false), _password(""), _userLimit(0)
+{
+}
+
+Channel::~Channel() {}
+
+std::string Channel::getName() const { return _name; }
+std::string Channel::getTopic() const { return _topic; }
+std::string Channel::getPassword() const { return _password; }
+size_t      Channel::getUserLimit() const { return _userLimit; }
+
+bool Channel::isMember(Client *client) const
+{
+    return std::find(_members.begin(), _members.end(), client) != _members.end();
+}
+
+bool Channel::isOperator(Client *client) const
+{
+    return std::find(_operators.begin(), _operators.end(), client) != _operators.end();
+}
+
+void Channel::addMember(Client *client)
+{
+    if (!isMember(client))
+    {
+        _members.push_back(client);
+    }
+}
+
+void Channel::removeMember(Client *client)
+{
+    std::vector<Client*>::iterator it = std::find(_members.begin(), _members.end(), client);
+    if (it != _members.end())
+    {
+        _members.erase(it);
+    }
+    
+    removeOperator(client);
+}
+
+void Channel::addOperator(Client *client)
+{
+    if (!isOperator(client))
+    {
+        _operators.push_back(client);
+    }
+}
+
+void Channel::removeOperator(Client *client)
+{
+    std::vector<Client*>::iterator it = std::find(_operators.begin(), _operators.end(), client);
+    if (it != _operators.end())
+    {
+        _operators.erase(it);
+    }
+}
--- include/Channel.hpp
+++ include/Channel.hpp
@@ -1,23 +1,44 @@
-
-
-#paragma once 
+#pragma once
 
 #include <string>
 #include <vector>
-#include "Client.hpp"
+#include "Client.hpp" 
 
-class Channel 
+class Channel
 {
     private:
-        std::string name;
-        std::string topic;
+        std::string             _name;
+        std::string             _topic;
+        
+        std::vector<Client*>    _members;
+        std::vector<Client*>    _operators; 
 
-        std::vector<Client*> members;
-        std::vector<Client*> operators; // admins 
+        bool                    _isInviteOnly;      // mode +i
+        bool                    _topicRestricted;   // mode +t
+        
+        std::string             _password;          // mode +k 
+        size_t                  _userLimit;         // mode +l 
 
-        bool    isInviteOnly; // mode +i 
-        bool    topicRestricted  // mode +t 
-        size_t                  userLimit; // mode +k
-        std::string             password; // mode +l
-        bool 
-}
+        std::vector<Client*>    _invitedClients;
+
+    public:
+    
+        Channel(const std::string &name);
+        ~Channel();
+
+        std::string getName() const;
+        std::string getTopic() const;
+        std::string getPassword() const;
+        size_t      getUserLimit() const;
+        
+     
+        void        addMember(Client *client);
+        void        removeMember(Client *client);
+        void        addOperator(Client *client);
+        void        removeOperator(Client *client);
+        
+        bool        isMember(Client *client) const;
+        bool        isOperator(Client *client) const;
+        
+        void        broadcastMessage(const std::string &message, Client *sender = NULL);// khass message itsafet l ga3 members, null bach message maywselch joj merat l nafs chakhes  
+};
