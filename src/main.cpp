f3a907acc60ef163ad923df8b2d59fb435bdfeaa
oussama7007 <oussamatachakourt@gmail.com>
2026-06-29 05:30:48 +0100

    [Si HAMOU] 2026-06-29 05:30 | Modified: ircserv,src/channel/Channel.o,src/client/Client.o,src/commands/InviteCommand.o,src/commands/Join.o,src/commands/Kick.o,src/commands/ModeCommand.o,src/commands/Nick.o,src/commands/PartCommand.o,src/commands/Pass.o,src/commands/PrivmsgCommand.o,src/commands/User.o,src/main.cpp,src/main.o,src/parser/Parser.o,src/server/Server.o

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,11 @@ int main(int argc, char **argv)
 
     int port = std::atoi(*(argv + 1));
     
-    if (port < 1024 || port > 65535)
-    {
-        std::cerr << "Error: Port must be a number between 1024 and 65535." << std::endl;
-        return 1;
-    }
+    // if (port < 1024 || port > 65535)
+    // {
+    //     std::cerr << "Error: Port must be a number between 1024 and 65535." << std::endl;
+    //     return 1;
+    // }
 
   
     std::string password = *(argv + 2);
